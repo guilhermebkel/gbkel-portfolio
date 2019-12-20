294feb9d59b0606e515b1253c6d3fbbc2f09ceb0
Guilherme <guilhermebromonschenkel@gmail.com>
2019-12-20 07:03:39 -0300

    chore(*): add plugins and default icon

---
+++ config/metadata.js
@@ -0,0 +1,27 @@
+module.exports = {
+	title: "Guilherme Bromonschenkel",
+	siteUrl: "https://guilherr.me",
+	description: "A software developer.",
+	social: [
+		{
+			name: "github",
+			url: "https://github.com/guilhermebkel"
+		},
+		{
+			name: "linkedin",
+			url: "https://www.linkedin.com/in/guilhermebkel"
+		},
+		{
+			name: "npm",
+			url: "https://www.npmjs.com/~guilhermebkel"
+		},
+		{
+			name: "wakatime",
+			url: "https://wakatime.com/@guilhermebkel"
+		},
+		{
+			name: "whatsapp",
+			url: "https://api.whatsapp.com/send?phone=5527996334936"
+		}
+	]
+}
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,46 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
+const siteMetadata = require("./config/metadata")
 
 module.exports = {
-  /* Your site config here */
+  siteMetadata,
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: "Guilherme Bromonschenkel",
+        short_name: "GuilhermeBkel",
+        start_url: "/",
+        background_color: "#f7f0eb",
+        theme_color: "#0598e3",
+        display: "standalone",
+        icon: "src/assets/icon.png"
+      }
+    },
+    {
+      resolve: "gatsby-plugin-nprogress",
+      options: {
+        color: "#0598e3",
+        showSpinner: true
+      }
+    },
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "UA-140879800-1",
+      }
+    },
+    {
+      resolve: "gatsby-plugin-prefetch-google-fonts",
+      options: {
+        fonts: [
+          {
+            family: "Lato",
+            variants: ["300", "500", "700"]
+          }
+        ]
+      }
+    },
+    "gatsby-plugin-offline",
+  ]
 }
