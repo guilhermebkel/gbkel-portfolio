9fc687c1fe6ce46d29e449afa15bed2408c0153a
Guilherme <guilhermebromonschenkel@gmail.com>
2019-12-22 19:13:20 -0300

    chore(introduction): improve particles bg

--- src/components/Introduction/index.js
+++ src/components/Introduction/index.js
@@ -6,47 +6,7 @@ import { Container, Title, Content, Description } from "./styles"
 const Introduction = () => {
 	return (
 		<Container>
-			<Particles 
-				params={{
-					"particles": {
-							"number": {
-									"value": 200,
-									"density": {
-											"enable": true,
-											"value_area": 1000
-									}
-							},
-							"line_linked": {
-									"enable": true,
-									"opacity": 0.05
-							},
-							"size": {
-									"value": 1
-							},
-							"opacity": {
-									"anim": {
-											"enable": true,
-											"speed": 1,
-											"opacity_min": 0.05
-									}
-							}
-					},
-					"interactivity": {
-							"events": {
-									"onclick": {
-											"enable": true,
-											"mode": "push"
-									}
-							},
-							"modes": {
-									"push": {
-											"particles_nb": 1
-									}
-							}
-					},
-					"retina_detect": true
-			}}
-			/>
+			<Particles />
 			<Content>
 				<Title>
 					Hello, I'm <b>Guilherme Bromonschenkel</b>.
