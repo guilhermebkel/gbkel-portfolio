3b026ccd1211db4baa1a625bbd3bc2d9bc08e713
Guilherme <guilhermebromonschenkel@gmail.com>
2020-02-13 06:31:59 -0300

    chore(*): update og image

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -8,7 +8,7 @@ export const onRenderBody = ({ setHeadComponents, setPostBodyComponents }) => {
 	setHeadComponents([
     <meta name="theme-color" content="#FFF" key="theme-color" />,
     <meta property="og:title" content={siteMetadata.title} key="og:title" />,
-    <meta property="og:image" content="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/9168d579801893.5d4ec91666bf0.png" key="og:image" />,
+    <meta property="og:image" content="logo.png" key="og:image" />,
     <meta property="og:url" content={siteMetadata.siteUrl} key="og:url"/>,
     <meta property="og:description" content={siteMetadata.description} key="og:description" />,
     <meta property="og:type" content="website" key="og:type" />,
