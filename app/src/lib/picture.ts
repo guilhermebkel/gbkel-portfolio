fd1e5423a73c8c86cf4e6baeceb2711015a72c02
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2020-10-09 07:36:02 -0300

    refactor(*): move picture resize method to lib

--- app/src/lib/picture.ts
+++ app/src/lib/picture.ts
@@ -1,8 +1,31 @@
-export type OptimizedPublicPictureUrlOptions = {
-	assetKey: string
-	baseURL?: string
+import path from "path"
+import sharp from "sharp"
+import fs from "fs"
+
+const ASSETS_FOLDER_PATH = path.join(process.cwd(), "src", "assets")
+
+export type ResizePictureAssetOptions = {
 	width?: number
 	height?: number
+	assetKey: string
+}
+
+export const optimizePictureAsset = async (
+	options: ResizePictureAssetOptions
+): Promise<Buffer> => {
+	const picturePath = path.join(ASSETS_FOLDER_PATH, options.assetKey)
+
+	const picture = await fs.promises.readFile(picturePath)
+
+	const optimizedPicture = await sharp(picture)
+		.resize(options.width, options.height)
+		.toBuffer()
+
+	return optimizedPicture
+}
+
+export type OptimizedPublicPictureUrlOptions = ResizePictureAssetOptions & {
+	baseURL?: string
 }
 
 export const optimizedPublicPictureUrl = (
--- app/src/pages/api/optimized-picture.ts
+++ app/src/pages/api/optimized-picture.ts
@@ -1,11 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next"
-import path from "path"
-import fs from "fs"
-import sharp from "sharp"
 
-import { OptimizedPublicPictureUrlOptions } from "@/lib/picture"
+import { optimizePictureAsset } from "@/lib/picture"
 
-const ASSETS_FOLDER_PATH = path.join(process.cwd(), "src", "assets")
 
 export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
 	try {
@@ -17,19 +13,11 @@ export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> 
 			return res.status(404).json({ error: "PictureAssetNotFound" })
 		}
 
-		const options: OptimizedPublicPictureUrlOptions = {
+		const optimizedPicture = await optimizePictureAsset({
 			assetKey,
 			height: isNaN(+height) ? null : +height,
 			width: isNaN(+width) ? null : +width
-		}
-		
-		const picturePath = path.join(ASSETS_FOLDER_PATH, assetKey)
-
-		const picture = await fs.promises.readFile(picturePath)
-
-		const optimizedPicture = await sharp(picture)
-			.resize(options.width, options.height)
-			.toBuffer()
+		})
 
 		res.setHeader("Content-Type", "image/png")
 		res.setHeader("Cache-Control", "public, immutable, no-transform, s-maxage=31536000, max-age=31536000")
