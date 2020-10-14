bc83db695e8fa32e0764c169444d71911528b5f2
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2020-10-14 08:12:27 -0300

    chore(*): add font as static file

--- app/src/layouts/Default/index.tsx
+++ app/src/layouts/Default/index.tsx
@@ -17,7 +17,7 @@ const DefaultLayout: React.FC = (props) => {
 				<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 				<link rel="icon" href="/favicon.png" />
 				<link id="app-manifest" rel="manifest" href="/manifest.json"></link>
-				<link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet"></link>
+				<link href="/css/font.css" rel="stylesheet"></link>
 				<meta property="og:image" content="/favicon.png" />
 			</Head>
 
