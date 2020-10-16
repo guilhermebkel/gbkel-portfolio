bb73dfb806fcdd1bbf8ba318091007155c84514b
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2020-10-16 07:43:42 -0300

    chore(*): use sharp as development dependencie

--- app/next.config.js
+++ app/next.config.js
@@ -1,6 +1,6 @@
 const withPWA = require("next-pwa")
 const withPlugins = require("next-compose-plugins")
-const withOptimizedPublicPictures = require("./src/_plugins/withOptimizedPublicPictures")
+const withOptimizedPublicPictures = require("./src/plugins/withOptimizedPublicPictures")
 
 const isDev = process.env.NODE_ENV === "development"
 
