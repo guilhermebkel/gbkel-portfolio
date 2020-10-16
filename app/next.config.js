5b9c0182f312acaf9debe9fa1cd5c5a599f5ef24
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2020-10-16 08:00:29 -0300

    chore(*): add plugin compose back

--- app/next.config.js
+++ app/next.config.js
@@ -1,17 +1,20 @@
 const withPWA = require("next-pwa")
+const withPlugins = require("next-compose-plugins")
 const withOptimizedPublicPictures = require("./src/plugins/withOptimizedPublicPictures")
 
 const isDev = process.env.NODE_ENV === "development"
 
-module.exports = withPWA(
-	withOptimizedPublicPictures({
+module.exports = withPlugins([
+	[withPWA, {
 		target: "serverless",
 		pwa: {
 			disable: isDev,
 			dest: "public"
-		},
+		}
+	}],
+	[withOptimizedPublicPictures, {
 		optimizedPublicPictures: {
 			disable: isDev
 		}
-	})
-)
+	}]
+])
