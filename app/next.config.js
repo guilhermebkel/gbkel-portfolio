f98d4fcf4a3105d82de8b0580accb52b99340f5b
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2020-10-16 06:56:44 -0300

    chore(*): add next plugins compose

--- app/next.config.js
+++ app/next.config.js
@@ -1,11 +1,14 @@
 const withPWA = require("next-pwa")
+const withPlugins = require("next-compose-plugins")
 
 const isDev = process.env.NODE_ENV === "development"
 
-module.exports = withPWA({
-	target: "serverless",
-	pwa: {
-		disable: isDev,
-		dest: "public"
-	}
-})
+module.exports = withPlugins([
+	[withPWA, {
+		target: "serverless",
+		pwa: {
+			disable: isDev,
+			dest: "public"
+		}
+	}],
+])
