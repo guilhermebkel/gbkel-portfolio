df916dafc7b68ffebd377e63200b5c3f2ebc0d6e
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2020-10-16 07:52:47 -0300

    chore(*): remove useless dependency

--- app/next.config.js
+++ app/next.config.js
@@ -1,20 +1,17 @@
 const withPWA = require("next-pwa")
-const withPlugins = require("next-compose-plugins")
-const withOptimizedPublicPictures = require("./src/plugins/_withOptimizedPublicPictures")
+const withOptimizedPublicPictures = require("./src/plugins/withOptimizedPublicPictures")
 
 const isDev = process.env.NODE_ENV === "development"
 
-module.exports = withPlugins([
-	[withPWA, {
+module.exports = withOptimizedPublicPictures(
+	withPWA({
 		target: "serverless",
 		pwa: {
 			disable: isDev,
 			dest: "public"
-		}
-	}],
-	[withOptimizedPublicPictures, {
+		},
 		optimizedPublicPictures: {
 			disable: isDev
 		}
-	}]
-])
+	})
+)
