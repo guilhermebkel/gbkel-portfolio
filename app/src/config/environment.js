05d8145632fb4350f6d57ff74deff360e7adcb0a
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2020-10-17 12:37:41 -0300

    chore(*): improve isDev validation

--- app/src/config/environment.js
+++ app/src/config/environment.js
@@ -1,6 +1,6 @@
 const environmentConfig = {
 	isServerDev: !process.env.AWS_REGION || process.env.NODE_ENV === "development",
-	isBrowserDev: globalThis.origin && globalThis.origin.includes?.("localhost"),
+	isBrowserDev: globalThis.origin && globalThis.origin.includes("localhost"),
 	isBrowser: globalThis.origin
 }
 
