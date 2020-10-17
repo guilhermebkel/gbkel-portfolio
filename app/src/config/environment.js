694d333de133586277ad8fe8b3bd8214d6ba64ae
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2020-10-17 12:27:20 -0300

    chore(*): improve dev validation

--- app/src/config/environment.js
+++ app/src/config/environment.js
@@ -1,5 +1,5 @@
 const environmentConfig = {
-	isDev: !process.env.AWS_REGION || process.env.NODE_ENV === "development"
+	isDev: !process.env.AWS_REGION || process.env.NODE_ENV === "development" || !globalThis["origin"]
 }
 
 module.exports = {
