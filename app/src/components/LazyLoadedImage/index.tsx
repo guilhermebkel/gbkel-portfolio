cddcedcde4132b9cc1cf0d42bc0a796d80c973a1
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2020-10-17 12:22:31 -0300

    chore(*): remove console log

--- app/src/components/LazyLoadedImage/index.tsx
+++ app/src/components/LazyLoadedImage/index.tsx
@@ -22,7 +22,6 @@ const LazyLoadedImage: React.FC<LazyLoadedImageProps> = (props) => {
 	}
 
 	const onImageVisible = (_: Element, imageRef: Element) => {
-		console.log(buildCDNUrl(src))
 		imageRef["src"] = buildCDNUrl(src)
 		imageRef["alt"] = alt
 	}
--- app/src/lib/cdn.ts
+++ app/src/lib/cdn.ts
@@ -2,6 +2,7 @@ import { cdnConfig } from "@/config/cdn"
 import { environmentConfig } from "@/config/environment"
 
 export const buildCDNUrl = (path: string): string => {
+	console.log(environmentConfig.isDev)
 	if (environmentConfig.isDev) {
 		return path
 	}
