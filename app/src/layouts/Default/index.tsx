caa01d6d30ffc5b525ce23a903c317f9da46335d
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2020-10-27 20:48:08 -0300

    chore(*): add preload to link

--- app/src/layouts/Default/index.tsx
+++ app/src/layouts/Default/index.tsx
@@ -7,22 +7,8 @@ import {
 
 import { buildCDNUrl } from "@/lib/cdn"
 
-import useDidMount from "@/hooks/useDidMount"
-
 const DefaultLayout: React.FC = (props) => {
 	const { children } = props
-
-	useDidMount(() => {
-		window.addEventListener("load", () => {
-			const href = buildCDNUrl("/css/font.css")
-
-			const font = document.createElement("link") 
-			font.setAttribute("rel", "stylesheet")
-			font.setAttribute("href", href)
-
-			document.head.appendChild(font)
-		})
-	})
 
 	return (
 		<>
@@ -30,6 +16,7 @@ const DefaultLayout: React.FC = (props) => {
 				<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 				<link rel="icon" href={buildCDNUrl("/favicon.png")} />
 				<link id="app-manifest" rel="manifest" href="/manifest.json"></link>
+				<link rel="preload" href={buildCDNUrl("/css/font.css")}></link>
 				<meta property="og:image" content={buildCDNUrl("/favicon.png")} />
 				<meta name="description" content="All info about a problem solver called Guilherme Mota."/>
 			</Head>
