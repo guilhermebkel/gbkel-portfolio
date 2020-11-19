72265e3290f06059a0072d3f78669106ca8e5590
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2020-11-19 11:55:42 -0300

    chore(meta-tag): break meta tags into components

--- app/src/components/MetaTag/index.tsx
+++ app/src/components/MetaTag/index.tsx
@@ -31,46 +31,86 @@ type MetaTagProps = {
 
 const MetaTag: React.FC<MetaTagProps> = (props) => {
 	const { page, share, app } = props
+	
+	const PrimaryMetaTags = (
+		<>
+			{page?.title && (
+				<>
+					<title>{page.title}</title>
+					<meta name="title" content={page.title} />
+				</>
+			)}
+			{page?.description && <meta name="description" content={page.description} />}
+		</>
+	)
+
+	const FacebookMetaTags = (
+		<>
+			{share?.type && <meta property="og:type" content={share.type} />}
+			{share?.url && <meta property="og:url" content={share.url} />}
+			{share?.title && <meta property="og:title" content={share.title} />}
+			{share?.description && <meta property="og:description" content={share.description} />}
+			{share?.imageSrc && <meta property="og:image" content={share.imageSrc} />}
+		</>
+	)
+
+	const TwitterMetaTags = (
+		<>
+			<meta property="twitter:card" content="summary_large_image" />
+			{share?.url && <meta property="twitter:url" content={share.url} />}
+			{share?.title && <meta property="twitter:title" content={share.title} />}
+			{share?.description && <meta property="twitter:description" content={share.description} />}
+			{share?.imageSrc && (
+				<>
+					<meta property="twitter:image" content={share.imageSrc} />
+					<meta property="twitter:image:src" content={share.imageSrc} />
+				</>
+			)}
+			{share?.readingTime && (
+				<>
+					<meta name="twitter:label1" {...({ value: "Reading time" })} />
+					<meta name="twitter:data1" {...({ value: share.readingTime })} />
+				</>
+			)}
+		</>
+	)
+
+	const SecondaryMetaTags = (
+		<>
+			{share?.publishDate && <meta property="article:published_time" content={share.publishDate} />}
+			{share?.authorName && <meta name="author" content={share.authorName} />}
+			{share?.authorUrl && (
+				<>
+					<link rel="author" href={share.authorUrl} />
+					<meta property="article:author" content={share?.authorUrl} />
+				</>
+			)}
+		</>
+	)
+
+	const AppMetaTags = (
+		<>
+			{app?.ios?.url && <meta property="al:ios:url" content={app.ios.url} />}
+			{app?.ios?.appStoreId && <meta property="al:ios:app_store_id" content={app.ios.appStoreId} />}
+			{app?.android?.package && <meta property="al:android:package" content={app.android.package} />}
+			{app?.android?.url && <meta property="al:android:url" content={app.android.url} />}
+			{share?.url && <meta property="al:web:url" content={share.url} />}
+			{app?.name && (
+				<>
+					<meta property="al:ios:app_name" content={app.name} />
+					<meta property="al:android:app_name" content={app.name} />
+				</>
+			)}
+		</>
+	)
 
 	return (
 		<>
-			{/* Primary Meta Tags */}
-			<title>{page?.title}</title>
-			<meta name="title" content={page?.title} />
-			<meta name="description" content={page?.description} />
-
-			{/* Open Graph / Facebook */}
-			<meta property="og:type" content={share?.type} />
-			<meta property="og:url" content={share?.url} />
-			<meta property="og:title" content={share?.title} />
-			<meta property="og:description" content={share?.description} />
-			<meta property="og:image" content={share?.imageSrc} />
-
-			{/* Twitter */}
-			<meta property="twitter:card" content="summary_large_image" />
-			<meta property="twitter:url" content={share?.url} />
-			<meta property="twitter:title" content={share?.title} />
-			<meta property="twitter:description" content={share?.description} />
-			<meta property="twitter:image" content={share?.imageSrc} />
-			<meta property="twitter:image:src" content={share?.imageSrc} />
-			<meta name="twitter:label1" {...({ value: "Reading time" })} />
-			<meta name="twitter:data1" {...({ value: share?.readingTime })} />
-
-			{/* Other */}
-			<meta property="article:published_time" content={share?.publishDate} />
-			<meta property="article:author" content={share?.authorUrl}></meta>
-			<meta name="author" content={share?.authorName}></meta>
-			<link rel="author" href={share?.authorUrl} />
-			<link rel="canonical" href={share?.url} />
-
-			{/* Mobile Apps */}
-			<meta property="al:ios:url" content={app?.ios?.url} />
-			<meta property="al:ios:app_store_id" content={app?.ios?.appStoreId} />
-			<meta property="al:ios:app_name" content={app?.name} />
-			<meta property="al:android:url" content={app?.android?.url} />
-			<meta property="al:android:app_name" content={app?.name} />
-			<meta property="al:android:package" content={app?.android?.package} />
-			<meta property="al:web:url" content={share.url} />
+			{PrimaryMetaTags}
+			{FacebookMetaTags}
+			{TwitterMetaTags}
+			{SecondaryMetaTags}
+			{AppMetaTags}
 		</>
 	)
 }
