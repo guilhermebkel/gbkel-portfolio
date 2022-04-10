2a97086167aae16197e4f8aba7d16bd62c95580f
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2022-04-10 18:03:59 -0300

    chore(*): add new debugger

--- app/src/config/environment.js
+++ app/src/config/environment.js
@@ -1,4 +1,8 @@
-console.log(process.env, globalThis.origin)
+console.log(process.env, globalThis.origin, globalThis.origin ? (
+	globalThis.origin && globalThis.origin.includes("localhost")
+) : (
+	!process.env.AWS_REGION || process.env.NODE_ENV === "development"
+))
 
 const environmentConfig = {
 	isDev: globalThis.origin ? (
