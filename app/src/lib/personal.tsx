8dd5e74621b0a9abd400dbb5624d17bd1c036bde
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2022-04-10 17:52:49 -0300

    feat(navbar): use correct url

--- app/src/lib/personal.tsx
+++ app/src/lib/personal.tsx
@@ -197,7 +197,7 @@ export const timeline: Timeline[] = [
 ]
 
 export const appUrls = {
-	home: environmentConfig.isDev ? "/" : "https://guilherr.me",
+	home: "https://guilherr.me",
 	blog: environmentConfig.isDev ? "/blog" :"https://blog.guilherr.me",
 	about: "https://about.guilherr.me",
 	skills: "https://about.guilherr.me/skills"
